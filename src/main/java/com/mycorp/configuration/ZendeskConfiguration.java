b8b2923d96859b8ed792aee7c40ac17a356eb4c2
sergio-alvarez <s.alvarez1980@gmail.com>
2018-06-10 21:25:30 +0200

    uncouple handler for http client, added new objectMapper to configuration to reuse

--- src/main/java/com/mycorp/configuration/ZendeskConfiguration.java
+++ src/main/java/com/mycorp/configuration/ZendeskConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,6 +13,19 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 @Configuration
 @ComponentScan(basePackages = { "com.mycorp.*" })
 public class ZendeskConfiguration {
+
+    @Bean
+    @Qualifier("zendesk")
+    public ObjectMapper objectMapperZendesk() {
+	ObjectMapper mapper = new ObjectMapper();
+	mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+	mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+	mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+	mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+	// To allow empty beans --> i.e. Ticket
+	mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+	return mapper;
+    }
 
     @Bean
     @Qualifier("onlyIndentOutput")
---
+++ src/main/java/com/mycorp/http/client/handler/BasicAsyncCompletionHandler.java
@@ -0,0 +1,78 @@
+package com.mycorp.http.client.handler;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycorp.http.client.exceptions.ZendeskException;
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.Response;
+
+public class BasicAsyncCompletionHandler<T> extends AsyncCompletionHandler<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(BasicAsyncCompletionHandler.class);
+
+    private final Class<T> clazz;
+    private final String name;
+    private final Class[] typeParams;
+
+    @Autowired
+    @Qualifier("zendesk")
+    private ObjectMapper mapper;
+
+    public BasicAsyncCompletionHandler(Class clazz, String name, Class... typeParams) {
+	this.clazz = clazz;
+	this.name = name;
+	this.typeParams = typeParams;
+    }
+
+    @Override
+    public T onCompleted(Response response) throws Exception {
+	logResponse(response);
+	if (isStatus2xx(response)) {
+	    if (typeParams.length > 0) {
+		JavaType type = mapper.getTypeFactory().constructParametricType(clazz, typeParams);
+		return mapper.convertValue(mapper.readTree(response.getResponseBodyAsStream()).get(name), type);
+	    }
+	    return mapper.convertValue(mapper.readTree(response.getResponseBodyAsStream()).get(name), clazz);
+	} else if (isRateLimitResponse(response)) {
+	    throw new ZendeskException(response.toString());
+	}
+	if (response.getStatusCode() == 404) {
+	    return null;
+	}
+	throw new ZendeskException(response.toString());
+    }
+
+    @Override
+    public void onThrowable(Throwable t) {
+	if (t instanceof IOException) {
+	    throw new ZendeskException(t);
+	} else {
+	    super.onThrowable(t);
+	}
+    }
+
+    private boolean isRateLimitResponse(Response response) {
+	return response.getStatusCode() == 429;
+    }
+
+    private void logResponse(Response response) throws IOException {
+	if (LOG.isDebugEnabled()) {
+	    LOG.debug("Response HTTP/{} {}\n{}", response.getStatusCode(), response.getStatusText(),
+		    response.getResponseBody());
+	}
+	if (LOG.isTraceEnabled()) {
+	    LOG.trace("Response headers {}", response.getHeaders());
+	}
+    }
+
+    private boolean isStatus2xx(Response response) {
+	return (response.getStatusCode() / 100) == 2;
+    }
+
+}
